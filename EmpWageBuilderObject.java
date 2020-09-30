1d08587775468bafece5e8b27ef8a68dbd8783ee
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-09-30 17:21:13 +0530

    [Deepanjan] add .uc9 is solved.

--- EmpWageBuilderObject.java
+++ EmpWageBuilderObject.java
@@ -2,13 +2,26 @@ public class EmpWageBuilderObject{
     public static final int PARTTIME=1;
     public static final int FULLTIME=2;
     
-    public static int computeEmpWage(String company,int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth){
+    private final String company;
+    private final int empRatePerHour;
+    private final int numOfWorkingDays;
+    private final int maxHoursPerMonth;
+    private int totalEmpWage;
+    
+    public EmpWageBuilderObject(String company,int empRatePerHour, 
+                                int numOfWorkingDays,int maxHoursPerMonth)  {
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+     }
+
+    public void computeEmpWage() {
     //variables
     int empHours=0;
     int totalEmpHours=0;
     int totalWorkingDays=0;
-    while(totalEmpHours<=maxHoursPerMonth && totalWorkingDays<numOfWorkingDays)
-    {
+    while(totalEmpHours<=maxHoursPerMonth && totalWorkingDays<numOfWorkingDays) {
      totalWorkingDays++;
      int empCheck=(int)Math.floor(Math.random()*10)%3;
      switch(empCheck){
@@ -23,12 +36,19 @@ public class EmpWageBuilderObject{
          }
    totalEmpHours+=empHours;
    System.out.println("Days :"+totalWorkingDays+" Hours :"+empHours);
-   }
-   int totalEmpWage=totalEmpHours*empRatePerHour;
-   System.out.println("Total Employee Wage for Company :"+company+ " is: "+totalEmpWage);
-   return totalEmpWage;
-   }
-    public static void main(String[] args){
-        computeEmpWage("Dmart", 20 ,2,10);
-        computeEmpWage("Reliance",10,4,20); }
+       }
+    totalEmpWage=totalEmpHours*empRatePerHour;
+    }
+   @Override
+    public String toString()   {
+           return "Total Emp Wage for Company: " +company+ " is: " + totalEmpWage;
+             }
+  public static void main(String[] args) {
+        EmpWageBuilderObject dMart = new EmpWageBuilderObject("Dmart",20,2,10);
+        EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance",10,4,20);
+        dMart.computeEmpWage();
+        System.out.println(dMart);
+        reliance.computeEmpWage();
+        System.out.println(reliance);
+        }
 }
