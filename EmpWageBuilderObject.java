b2e6b42237aea459be9947b086c96c0ab776f0ec
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-09-30 06:51:44 +0530

    [Deepanjan] add .uc5 is solved.

--- EmpWageBuilderObject.java
+++ EmpWageBuilderObject.java
@@ -1,26 +1,33 @@
 public class EmpWageBuilderObject
-{
-   final static int PARTTIME=1;
-   final static int FULLTIME=2;
-   final static int EmpRatePerHour=20;
+ {
+  static final int PARTTIME=1;
+  static final int FULLTIME=2;
+  static final int EmpRatePerHour=20;
+  static final int NumberOfWorkDays=2;
   public static void main(String args[])
-   {
-	//variables
-	int empHours=0;
-	int empWage=0;
-	int empCheck=(int)Math.floor(Math.random()*10)%3;
-	switch(empCheck)
-	{
-	    case FULLTIME:
-                 empHours=8;
-            break;
-	    case PARTTIME:
-                 empHours=4;
-            break;
-            default:
-                empHours=0;
-         }
-      empWage=empHours*EmpRatePerHour;
-     System.out.println("Employee Wage :"+empWage);
+    {
+     //variables
+     int empHours=0;
+     int empWage=0;
+     int totalWage=0;
+     for(int day=0;day<NumberOfWorkDays;day++)
+     {
+      int empCheck=(int)Math.floor(Math.random()*10)%3;
+       switch(empCheck)
+       {
+       case FULLTIME:
+            empHours=8;
+       break;
+       case PARTTIME:
+            empHours=4;
+        break;
+       default:
+            empHours=0;
+        }
+       empWage=empHours*EmpRatePerHour;
+       totalWage+=empWage;
+       System.out.println("Employee Wage :"+empWage);
+      }
+     System.out.println("Total Employee Wage :"+totalWage);
     }
 }
