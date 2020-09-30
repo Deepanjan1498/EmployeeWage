c117b9c8e7cbb8d1818f897e0fa56f0e389c87f6
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-09-30 06:59:42 +0530

    [Deepanjan] add .uc6 is solved.

--- EmpWageBuilderObject.java
+++ EmpWageBuilderObject.java
@@ -1,33 +1,35 @@
 public class EmpWageBuilderObject
- {
-  static final int PARTTIME=1;
-  static final int FULLTIME=2;
-  static final int EmpRatePerHour=20;
-  static final int NumberOfWorkDays=2;
-  public static void main(String args[])
-    {
-     //variables
-     int empHours=0;
-     int empWage=0;
-     int totalWage=0;
-     for(int day=0;day<NumberOfWorkDays;day++)
-     {
-      int empCheck=(int)Math.floor(Math.random()*10)%3;
-       switch(empCheck)
-       {
-       case FULLTIME:
-            empHours=8;
-       break;
-       case PARTTIME:
-            empHours=4;
-        break;
-       default:
-            empHours=0;
-        }
-       empWage=empHours*EmpRatePerHour;
-       totalWage+=empWage;
-       System.out.println("Employee Wage :"+empWage);
-      }
-     System.out.println("Total Employee Wage :"+totalWage);
-    }
+{
+static final int PARTTIME=1;
+static final int FULLTIME=2;
+static final int EmpRatePerHour=20;
+static final int NumberOfWorkDays=2;
+static final int MaxWorkHrs=10;
+public static void main(String[] args)
+{
+//variables
+int empHours=0;
+int totalHours=0;
+int totalWorkingDays=0;
+while(totalHours<=MaxWorkHrs && totalWorkingDays<NumberOfWorkDays)
+{
+totalWorkingDays++;
+int empCheck=(int)Math.floor(Math.random()*10)%3;
+switch(empCheck)
+{
+case FULLTIME:
+empHours=8;
+break;
+case PARTTIME:
+empHours=4;
+break;
+default:
+empHours=0;
+}
+totalHours+=empHours;
+System.out.println("Days :"+totalWorkingDays+" Hours :"+empHours);
+}
+int totalWage=totalHours*EmpRatePerHour;
+System.out.println("Total Employee Wage :"+totalWage);
+}
 }
