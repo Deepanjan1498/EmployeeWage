dced2bd85f084f4ceae7ff6b56cbcd4de30f565c
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-09-30 06:22:39 +0530

    [Deepanjan] add .uc2 is solved.

--- EmpWageBuilderObject.java
+++ EmpWageBuilderObject.java
@@ -1,13 +1,19 @@
 public class EmpWageBuilderObject
 {
-public static void main(String[] args)
-{ //Constants
-int FULLTIME = 1;
-//Computation
-double empCheck = Math.floor(Math.random() * 10) % 2;
-if(empCheck == FULLTIME)
-System.out.println("Employee is Present");
-else
-System.out.println("Employee is Absent");
-}
+   public static void main(String args[])
+  {
+   //constants
+   int FULLTIME=1;
+   int EmpRatePerHour=20;
+   //variables
+   int empHours=0;
+   int empWage=0;
+   int empCheck=(int)(Math.random()*10)%2;
+   if(empCheck==FULLTIME)
+      empHours=8;
+   else
+      empHours=0;
+   empWage=empHours*EmpRatePerHour;
+   System.out.println("Employee Wage :"+empWage);
+   }
 }
