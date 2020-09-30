9934b3d4432dc7bcedd8c8e8ee28931ebb190f84
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-09-30 07:18:00 +0530

    [Deepanjan] add .uc7 is solved.

--- EmpWageBuilderObject.java
+++ EmpWageBuilderObject.java
@@ -1,35 +1,36 @@
-public class EmpWageBuilderObject
-{
-static final int PARTTIME=1;
-static final int FULLTIME=2;
-static final int EmpRatePerHour=20;
-static final int NumberOfWorkDays=2;
-static final int MaxWorkHrs=10;
-public static void main(String[] args)
-{
-//variables
-int empHours=0;
-int totalHours=0;
-int totalWorkingDays=0;
-while(totalHours<=MaxWorkHrs && totalWorkingDays<NumberOfWorkDays)
-{
-totalWorkingDays++;
-int empCheck=(int)Math.floor(Math.random()*10)%3;
-switch(empCheck)
-{
-case FULLTIME:
-empHours=8;
-break;
-case PARTTIME:
-empHours=4;
-break;
-default:
-empHours=0;
-}
-totalHours+=empHours;
-System.out.println("Days :"+totalWorkingDays+" Hours :"+empHours);
-}
-int totalWage=totalHours*EmpRatePerHour;
-System.out.println("Total Employee Wage :"+totalWage);
-}
+public class EmpWageBuilderObject{
+    public static final int PARTTIME=1;
+    public static final int FULLTIME=2;
+    public static final int EmpRatePerHour=20;
+    public static final int NumberOfWorkDays=2;
+    public static final int MaxWorkHrs=10;
+
+    public static int computeEmpWage(){
+    //variables
+    int empHours=0;
+    int totalHours=0;
+    int totalWorkingDays=0;
+    while(totalHours<=MaxWorkHrs && totalWorkingDays<NumberOfWorkDays)
+    {
+     totalWorkingDays++;
+     int empCheck=(int)Math.floor(Math.random()*10)%3;
+     switch(empCheck){
+     case FULLTIME:
+          empHours=8;
+     break;
+     case PARTTIME:
+          empHours=4;
+     break;
+     default:
+          empHours=0;
+     }
+   totalHours+=empHours;
+   System.out.println("Days :"+totalWorkingDays+" Hours :"+empHours);
+   }
+   int totalWage=totalHours*EmpRatePerHour;
+   System.out.println("Total Employee Wage :"+totalWage);
+   return totalWage;
+   }
+    public static void main(String[] args){
+        computeEmpWage(); }
 }
